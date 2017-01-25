acda52d71a4cf1f856126b17879dfe1710d0acb2
Aidan <aidanf2020@gmail.com>
2017-01-25 23:42:21 +0000

    Add ball collector code to subsystem

--- src/org/usfirst/frc/team233/robot/subsystems/BallCollector.java
+++ src/org/usfirst/frc/team233/robot/subsystems/BallCollector.java
@@ -1,10 +1,20 @@
 package org.usfirst.frc.team233.robot.subsystems;
 
+import org.usfirst.frc.team233.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+//import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class BallCollector extends Subsystem{
-
+public class BallCollector<Collect> extends Subsystem{
+	
+	//Collector Motor Definition
+	private SpeedController collectorMotor = new Talon(RobotMap.collectorMotorPort);
+	
 	public BallCollector() {
+		
 		// TODO Auto-generated constructor stub
 	}
 
@@ -13,5 +23,24 @@ public class BallCollector extends Subsystem{
 		// TODO Auto-generated method stub
 		
 	}
-
+	public void collect() {
+		
+		//Sets Motor Power To 1 
+		collectorMotor.set(1);
+		 
+	}
+	public void eject() {
+		
+		//Sets Motor Power To 1 
+		collectorMotor.set(-1);
+	}
+	
+		
+	public void stop() {
+		
+		//Sets Motor Power To 1 
+		collectorMotor.set(0);
+	}
+	
 }
+
