ccba04fc0f28c84a8163203aa2e51c6474111249
Antonio Teijeiro <aeteijeiro@miners.utep.edu>
2017-01-26 00:12:19 +0000

    Added code for shooter wheel

--- src/org/usfirst/frc/team233/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team233/robot/subsystems/Shooter.java
@@ -4,6 +4,9 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Shooter extends Subsystem {
 
+	private SpeedController shooter = new Talon(RobotMap.shooterMotorPort);
+	//private boolean isSpinning;
+	private double motorSpeed = 1.0;
 	public Shooter() {
 		// TODO Auto-generated constructor stub
 	}
@@ -12,6 +15,30 @@ public class Shooter extends Subsystem {
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
 		
+	}
+		public void spin() {
+		// TODO Auto-generated method stub
+		//double throttle = shooterJoystick.getRawAxis(8);
+		//System.out.println(throttle);
+		//double limitedThrottle = limit(throttle);
+		//limitedThrottle = limitedThrottle * limitedThrottle;
+//		if (isSpinning){
+//			
+//			shooter.set(0);
+//			isSpinning = false;
+//		
+//		} else {
+			shooter.set(motorSpeed);
+//			isSpinning = true;
+//			
+//		}
+			
+	
+		
+	}
+	
+	public void stop(){
+		shooter.set(0);
 	}
 
 }
