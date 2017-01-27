1d9c3ceda0567041b0c22f8d487ceea1e914723c
Antonio Teijeiro <aeteijeiro@miners.utep.edu>
2017-01-27 00:32:04 +0000

    Update SpinStop.java

--- src/org/usfirst/frc/team233/robot/commands/SpinStop.java
+++ src/org/usfirst/frc/team233/robot/commands/SpinStop.java
@@ -1,32 +1,46 @@
 package org.usfirst.frc.team233.robot.commands;
 
+//import java.util.concurrent.TimeUnit;
+
 import org.usfirst.frc.team233.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 public class SpinStop extends Command {
-	
+
 	public SpinStop() {
 		// TODO Auto-generated constructor stub
-		requires(Robot.shooterWheel);
+
 	}
 
 	// Called repeatedly when this Command is scheduled to run
 	@Override
 	protected void execute() {
-		//System.out.println("TankDrive Execute!!!");
-		Robot.shooterWheel.stop();;
+		// System.out.println("TankDrive Execute!!!");
+		//System.out.println("going to spin");
+	//	try {
+		//	TimeUnit.SECONDS.sleep(1);
+		//} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			//System.out.println("time delay isnt working");
+		//}
+
+		Robot.shooterWheel.stop();
+
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
 	@Override
 	protected boolean isFinished() {
 		return true; // Runs until interrupted
+		
 	}
 
 	// Called once after isFinished returns true
 	@Override
 	protected void end() {
-		
+		Robot.shooterWheel.stop();
+
 	}
+
 }
