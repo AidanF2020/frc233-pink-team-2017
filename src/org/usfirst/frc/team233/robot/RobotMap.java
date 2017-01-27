89427a23644d365a32000235dea6535ae60d6c99
Antonio Teijeiro <aeteijeiro@miners.utep.edu>
2017-01-27 00:26:20 +0000

    Update RobotMap.java

--- src/org/usfirst/frc/team233/robot/RobotMap.java
+++ src/org/usfirst/frc/team233/robot/RobotMap.java
@@ -16,22 +16,48 @@ public class RobotMap {
 	// number and the module. For example you with a rangefinder:
 	// public static int rangefinderPort = 1;
 	// public static int rangefinderModule = 1;
-	
 	public static int leftFrontMotorPort = 0;
 	public static int leftBackMotorPort = 1;
 	public static int rightFrontMotorPort = 2;
 	public static int rightBackMotorPort = 3;
 	public static int shooterMotorPort = 4;
-	public static int collectorMotorPort = 5;
-	public static int ropeClimberMotorPort = 6;
-	
+	public static int shooterIndexerPort = 5;
 	
 	public static int baseJoystickPort = 0;
 	public static int shooterJoystickPort = 1;
 	
-	public static int leftEncoderAPort = 0;
-	public static int leftEncoderBPort = 1;
-	public static int rightEncoderAPort = 3;
-	public static int rightEncoderBPort = 4;
-	
+	/** Encoder enum. Place any new encoder definition as follow:
+	 * <Encoder_Name>(Channel A port, Channel B port).
+	 * 
+	 * NOTE: port definition for the encoders should
+	 * be added to the encoder definition area*/
+	public static enum Encoders {
+		/* ENCODER DEFINITION AREA */
+		
+		// Drive train encoders
+		FrontLeft(0,1),
+		BackLeft(2,3),
+		FrontRight(4,5),
+		BackRight(6,7);
+		/* ADD NEW ENCODERS HERE */
+		
+		//=====================================
+		/* DO NOT TOUCH THIS PART OF
+		 * THE CODE */
+		private final int channelA;
+		private final int channelB;
+		
+		Encoders(int channelA, int channelB) {
+			this.channelA = channelA;
+			this.channelB = channelB;
+		}
+		
+		public int channelA() {
+			return this.channelA;
+		}
+		
+		public int channelB() {
+			return this.channelB;
+		}
+	}
 }
