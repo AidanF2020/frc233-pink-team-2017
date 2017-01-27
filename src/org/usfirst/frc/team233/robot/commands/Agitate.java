b61f70112a3c5f9a7f086147bfbdb54ec6b9d58d
Meriel Stein <meriel.k.stein@gmail.com>
2017-01-27 20:01:03 +0000

    Add new file

---
+++ src/org/usfirst/frc/team233/robot/commands/Agitate.java
@@ -0,0 +1,24 @@
+package org.usfirst.frc.team233.robot.commands;
+
+import org.usfirst.frc.team233.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class Agitate extends Command {
+	
+	public Agitate(){
+		requires(Robot.hopper);
+	}
+	
+	@Override
+	protected void execute(){
+		Robot.hopper.agitate(Robot.oi.getShooterJoystick());
+	}
+
+
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+
+}
