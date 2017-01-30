c75b17af2768e8a6b2110017db4f578feefea624
Meriel Stein <meriel.k.stein@gmail.com>
2017-01-30 02:05:32 +0000

    Update OI.java

--- src/org/usfirst/frc/team233/robot/OI.java
+++ src/org/usfirst/frc/team233/robot/OI.java
@@ -10,13 +10,6 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
-//import org.usfirst.frc.team233.robot.commands.Autonomous;
-//import org.usfirst.frc.team233.robot.commands.OpenClaw;
-//import org.usfirst.frc.team233.robot.commands.Pickup;
-//import org.usfirst.frc.team233.robot.commands.Place;
-//import org.usfirst.frc.team233.robot.commands.PrepareToPickup;
-//import org.usfirst.frc.team233.robot.commands.SetElevatorSetpoint;
-
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
@@ -58,53 +51,16 @@ public class OI {
 		JoystickButton shooterleftJoystickPress = new JoystickButton(shooter, 11);
 		JoystickButton shooterrightJoystickPress = new JoystickButton(shooter, 12);
 		int shooterDpad = shooter.getPOV();
-		
-		
-		// Connect the buttons to commands
-		//d_up.whenPressed(new SetElevatorSetpoint(0.2));
-		//d_down.whenPressed(new SetElevatorSetpoint(-0.2));
-		//d_right.whenPressed(new CloseClaw());
-		//d_left.whenPressed(new OpenClaw());
 
-		//r1.whenPressed(new PrepareToPickup());
-		//r2.whenPressed(new Pickup());
-		//l1.whenPressed(new Place());
-		//l2.whenPressed(new Autonomous());
-		
-		shooterRightBumper.whileHeld(new SpinUp());
-		shooterRightBumper.whenReleased(new SpinStop());
-		
-		/** READ THIS!!!
-		 * From my understanding we are going to implement
-		 * the shooting capability in one action, not two
-		 * as we previously thought. So for testing purposes
-		 * we are going to put these command in the right bumper
-		 * to test the logic. BUT we will need to create a 
-		 * class that extends the CommandGroup class to implement
-		 * a set of commands that will:
-		 *	(1) Set the shooter wheel to the correct speed
-		 *	(2) Hopper will feed the balls
-		 *	(3) Shoot the balls
-		 *	(4) Stop all these actions once the driver 
-		 *		releases the trigger
-		 *
-		 * Also note that we are going to use the triggers
-		 * instead of the bumpers to perform the shooting.
-		 * 
-		 * TODO - Test these commands to see if they work.
-		 * */
-		rightBumper.whileHeld(new SpinUp());
-		rightBumper.whenReleased(new SpinStop());
-
-//		shooterRightBumper.whileHeld(new SpinUp());
-//		shooterRightBumper.whenReleased(new SpinStop());
-
-		
+        // Base button-command pairs (responsible for driving & collecting)
 		rightTrigger.whileHeld(new CollectBalls());
 		rightTrigger.whenReleased(new StopCollector());
-		
 		leftTrigger.whileHeld(new EjectBalls());
 		leftTrigger.whenReleased(new StopCollector());
+		
+		// Shooter button-command pairs (responsible for shooting & climbing)
+		shooterRightBumper.whenPressed(new SpinUp());
+		shooterRightTrigger.whileHeld(new Shoot());
 	}
 	
 	public Joystick getBaseJoystick() {
