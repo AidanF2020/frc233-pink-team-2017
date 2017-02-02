ac573ba750adb555629dfe3253179958c3a4fa0e
Antonio Teijeiro <aeteijeiro@miners.utep.edu>
2017-02-02 15:18:58 +0000

    Replace Shooter.java

--- src/org/usfirst/frc/team233/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team233/robot/subsystems/Shooter.java
@@ -20,7 +20,7 @@ public class Shooter extends Subsystem {
 
 	public FlyWheel flywheel;
 	public Indexer indexer;
-	PIDController pid;
+	public PIDController pid;
 	
 	public Shooter(){
 		flywheel = new FlyWheel();
@@ -31,7 +31,9 @@ public class Shooter extends Subsystem {
 	protected void initDefaultCommand() {
 	}
 	
-	public void shoot(int shooterDpadPos){
+	//public void shoot(int shooterDpadPos){
+	public void shoot(){
+		flywheel.flywheelMotor.set(1);
 		if(Robot.shooter.flywheel.motorSpeedEqualsSetSpeed()){
 			indexer.releaseBall();
 		} else {
@@ -50,7 +52,7 @@ public class Shooter extends Subsystem {
 		public double flywheelSpeed = .8;
 		private int simulatedEncoderCount = 0;
 		private double tolerance = 0.1;
-		private Encoder encoder = new Encoder(0, 0);
+		//private Encoder encoder = new Encoder(0, 0);
 		
 		/* Calculate the distance each pulse in the encoder equals to.
 		 * Equation: (Wheel Diameter x Pi) / Number of pulses per encoder revolution */
@@ -106,14 +108,14 @@ public class Shooter extends Subsystem {
 		
 		/** Reset all encoders. */
 		public void resetEncoder() {
-			encoder.reset();
+			//encoder.reset();
 		}
 		
 		/** Setup encoders before use. */
 		public void setupEncoder() {
-			encoder.setDistancePerPulse(distancePerPulse);
+			//encoder.setDistancePerPulse(distancePerPulse);
 			//leftEncoder.setPIDSourceType(PIDSourceType.kDisplacement);
-			SmartDashboard.putData("Flywheel Encoder", encoder);
+			//SmartDashboard.putData("Flywheel Encoder", encoder);
 		}
 		
 		public double getFlywheelSetSpeed(){
@@ -121,7 +123,8 @@ public class Shooter extends Subsystem {
 		}
 		
 		public double getFlywheelMotorSpeed(){
-			return encoder.getRate();
+			//return encoder.getRate();
+			return 0;
 		}
 		
 		public void adjustFlywheelSpeed(){
