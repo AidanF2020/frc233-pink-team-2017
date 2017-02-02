acfd2bd85293a7165e17bfcda1138bb8902f9c94
Antonio Teijeiro <aeteijeiro@miners.utep.edu>
2017-02-02 15:31:23 +0000

    Replace SpinUp.java

--- src/org/usfirst/frc/team233/robot/commands/SpinUp.java
+++ src/org/usfirst/frc/team233/robot/commands/SpinUp.java
@@ -1,18 +1,11 @@
+
 package org.usfirst.frc.team233.robot.commands;
-
-//import java.util.concurrent.TimeUnit;
-
 import org.usfirst.frc.team233.robot.Robot;
-
 import edu.wpi.first.wpilibj.command.Command;
 
 public class SpinUp extends Command {
-
-	public SpinUp() {
-		// TODO Auto-generated constructor stub
-
-	}
-
+	public SpinUp() {}
+	
 	// Called repeatedly when this Command is scheduled to run
 	@Override
 	protected void execute() {
@@ -24,11 +17,12 @@ public class SpinUp extends Command {
 			// TODO Auto-generated catch block
 			//System.out.println("time delay isnt working");
 		//}
+		
+		//Dpad position == 8 when nothing pressed
 		if(Robot.oi.shooterDpad != 8){
 			Robot.shooter.flywheel.adjustFlywheelSpeed();
 		}
 		Robot.shooter.flywheel.spin();
-		Robot.shooterWheel.spin();
 		//try {
 		//	TimeUnit.SECONDS.sleep(2);
 		//} catch (InterruptedException e) {
@@ -36,21 +30,16 @@ public class SpinUp extends Command {
 		//}
 		
 		//Robot.indexerWheel.spin();
-
 	}
-
 	// Make this return true when this Command no longer needs to run execute()
 	@Override
 	protected boolean isFinished() {
 		return true; // Runs until interrupted
-		
 	}
-
+	
 	// Called once after isFinished returns true
 	@Override
 	protected void end() {
 		//Robot.shooterWheel.stop();
-
 	}
-
 }
