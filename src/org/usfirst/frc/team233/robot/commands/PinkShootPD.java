e8f57da2fce2bdd4f87643dcfd87725e9b8658f3
Derek Perdomo <dperdomo13@yahoo.com>
2017-02-03 22:32:26 +0000

    Speed control PID

---
+++ src/org/usfirst/frc/team233/robot/commands/PinkShootPD.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team233.robot.subsystems;
+
+import org.usfirst.frc.team233.robot.Robot;
+import org.usfirst.frc.team233.robot.RobotMap;
+import org.usfirst.frc.team233.robot.Range;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class PinkShootPD{
+
+    private PinkShootPD() {
+    }
+
+    // Use a PD to determine a motor command, which has a range of -1 to 1 (-1=rev; 0=stop; 1=fwd)
+    public static double getSpeedCmd( double targetSpeed, double Kp) {
+    	double currentSpeed = Robot.shooterWheel.getRate();
+    	double error = (targetSpeed - currentSpeed);
+        double speedCmd;
+        System.out.println("inside getSpeedCmd, currentSpeed: "+currentSpeed);
+        speedCmd = ((Kp * error) + targetSpeed);
+
+        return speedCmd;
+    } 
+}
+
