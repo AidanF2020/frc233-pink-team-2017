9c10bce86429c53e9d19851dfcc5cbc86cc99f9e
Jonathan J Serrano <serrano.otero.j@gmail.com>
2017-03-02 09:04:52 -0500

    Updated code to fix merge issues.

--- src/org/usfirst/frc/team233/robot/autonomous/AutoGearRoutine2.java
+++ src/org/usfirst/frc/team233/robot/autonomous/AutoGearRoutine2.java
@@ -20,42 +20,70 @@ public class AutoGearRoutine2 extends CommandGroup {
 			/* RED ALLIANCE AUTONOMOUS CODE */
 			addSequential(new WaitCommand(Robot.delayTime));
 			// back up to spring tip2
-			addSequential(new PinkNavigate(-62, 0, 0.7));		
-//			addSequential(new PinkNavigate(24,0, 1));
-//			addSequential(new PinkNavigate(24,60, 1));
-//			addSequential(new PinkNavigate(24,-60, 1));
-			
+			addSequential(new PinkNavigate(-65, 0, 0.7));		
 			//ease onto spear
-			addSequential(new PinkNavigate(-71, 0, 0.5));
+			addSequential(new PinkNavigate(-74, 0, 0.5));
 			// go forward off of spear and hopper
 			addSequential(new WaitCommand(2));
-			addSequential(new PinkNavigate(0, 0, 0.7));
-			addSequential(new PinkNavigate(0, 135, 1));
+			addSequential(new PinkNavigate(-20, 0, 0.7)); // Don't go all the way to the end to turn
+			addSequential(new PinkNavigate(-20, 135, 1));
 			addSequential(new PinkNavigate(200, 135, 1));
 			addSequential(new PinkNavigate(200, 180, 1));
 			addSequential(new PinkNavigate(224, 180, 0.7));
-			addSequential(new PinkNavigate(224, 90, 0.7));
-			addSequential (new HoldPosition(224,90,0.7));
+			
+			
+			// DERECK'S CODE
+			// back up to spring tip2
+//			addSequential(new PinkNavigate(-62, 0, 0.7));		
+////			addSequential(new PinkNavigate(24,0, 1));
+////			addSequential(new PinkNavigate(24,60, 1));
+////			addSequential(new PinkNavigate(24,-60, 1));
+//			
+//			//ease onto spear
+//			addSequential(new PinkNavigate(-71, 0, 0.5));
+//			// go forward off of spear and hopper
+//			addSequential(new WaitCommand(2));
+//			addSequential(new PinkNavigate(0, 0, 0.7));
+//			addSequential(new PinkNavigate(0, 135, 1));
+//			addSequential(new PinkNavigate(200, 135, 1));
+//			addSequential(new PinkNavigate(200, 180, 1));
+//			addSequential(new PinkNavigate(224, 180, 0.7));
+//			addSequential(new PinkNavigate(224, 90, 0.7));
+//			addSequential (new HoldPosition(224,90,0.7));
 		} else {
 			/* BLUE ALLIANCE AUTONOMOUS CODE */
 			addSequential(new WaitCommand(Robot.delayTime));
 			// back up to spring tip2
-			addSequential(new PinkNavigate(-62, 0, 0.7));		
-//			addSequential(new PinkNavigate(24,0, 1));
-//			addSequential(new PinkNavigate(24,60, 1));
-//			addSequential(new PinkNavigate(24,-60, 1));
-			
+			addSequential(new PinkNavigate(-65, 0, 0.7));	
 			//ease onto spear
-			addSequential(new PinkNavigate(-71, 0, 0.5));
+			addSequential(new PinkNavigate(-74, 0, 0.5));
 			// go forward off of spear and hopper
 			addSequential(new WaitCommand(2));
-			addSequential(new PinkNavigate(0, 0, 0.7));
-			addSequential(new PinkNavigate(0, 135, 1));
+			addSequential(new PinkNavigate(-20, 0, 0.7)); // Don't go all the way to the end to turn
+			addSequential(new PinkNavigate(-20, 135, 1));
 			addSequential(new PinkNavigate(200, 135, 1));
 			addSequential(new PinkNavigate(200, 180, 1));
 			addSequential(new PinkNavigate(224, 180, 0.7));
-			addSequential(new PinkNavigate(224, 90, 0.7));
-			addSequential (new HoldPosition(224,90,0.7));
+			
+			
+			//DERECK'S CODE
+			// back up to spring tip2
+//			addSequential(new PinkNavigate(-62, 0, 0.7));		
+////			addSequential(new PinkNavigate(24,0, 1));
+////			addSequential(new PinkNavigate(24,60, 1));
+////			addSequential(new PinkNavigate(24,-60, 1));
+//			
+//			//ease onto spear
+//			addSequential(new PinkNavigate(-71, 0, 0.5));
+//			// go forward off of spear and hopper
+//			addSequential(new WaitCommand(2));
+//			addSequential(new PinkNavigate(0, 0, 0.7));
+//			addSequential(new PinkNavigate(0, 135, 1));
+//			addSequential(new PinkNavigate(200, 135, 1));
+//			addSequential(new PinkNavigate(200, 180, 1));
+//			addSequential(new PinkNavigate(224, 180, 0.7));
+//			addSequential(new PinkNavigate(224, 90, 0.7));
+//			addSequential (new HoldPosition(224,90,0.7));
 		}
 	}
 }
--- src/org/usfirst/frc/team233/robot/autonomous/AutoGearRoutine3.java
+++ src/org/usfirst/frc/team233/robot/autonomous/AutoGearRoutine3.java
@@ -22,7 +22,7 @@ public class AutoGearRoutine3 extends CommandGroup {
 			addSequential(new PinkNavigate(-84,-60,1));
 	        addSequential(new PinkNavigate(-126,-60,0.7));
 	        //go to base line
-			//addSequential(new WaitCommand(2));
+			addSequential(new WaitCommand(2));
 			addSequential(new PinkNavigate(-90, -60, 0.7));
 			addSequential(new PinkNavigate(-90, 30, 0.7));
 			addSequential(new PinkNavigate(-200, 30, 0.7));
@@ -32,44 +32,31 @@ public class AutoGearRoutine3 extends CommandGroup {
 		else {
 			/* BLUE ALLIANCE AUTONOMOUS CODE */
 			
-			//back up until you're on peg's angle
-//			addSequential(new PinkNavigate(-95, 0, 1));
-//			//turn 60 degrees to back up to tip of peg
-//			addSequential(new PinkNavigate(-95, -60, 0.7));
-//			addSequential(new PinkNavigate(-112, -60, 0.5));
-//			addSequential(new WaitCommand(1.0));
-//			//go to the hopper
-//			addSequential(new PinkNavigate(-80, -60, 1));
-//			addSequential(new PinkNavigate(-80, -180, 0.7));
-//			addSequential(new PinkNavigate(-5, -180, 0.7));
-//			addSequential(new PinkNavigate(-5, -180, 0.7));
 			addSequential(new WaitCommand(Robot.delayTime));
-			addSequential(new PinkNavigate(-72.5,0,1));
-			addSequential(new PinkNavigate(-72.5,60,1));
-			addSequential(new PinkNavigate(-137.5,60,1));
-			addSequential(new PinkNavigate(-140,60,0.5));
-			//go to base line
-			addSequential(new WaitCommand(2));
-			addSequential(new PinkNavigate(-121, -60, 0.7));
-			addSequential(new PinkNavigate(-121, 0, 0.7));
-			addSequential(new PinkNavigate(-300, 0, 0.7));
-			addSequential (new HoldPosition(-300,0,0.7));
+			//back up until you're on peg's angle
+			addSequential(new PinkNavigate(-95, 0, 1));
+			//turn 60 degrees to back up to tip of peg
+			addSequential(new PinkNavigate(-95, -60, 0.7));
+			addSequential(new PinkNavigate(-112, -60, 0.5));
+			addSequential(new WaitCommand(1.0));
+			//go to the hopper
+			addSequential(new PinkNavigate(-80, -60, 1));
+			addSequential(new PinkNavigate(-80, -180, 0.7));
+			addSequential(new PinkNavigate(-5, -180, 0.7));
+			addSequential(new PinkNavigate(-5, -180, 0.7));
+			
+			//DERECK'S CODE
+			
+//			addSequential(new PinkNavigate(-72.5,0,1));
+//			addSequential(new PinkNavigate(-72.5,60,1));
+//			addSequential(new PinkNavigate(-137.5,60,1));
+//			addSequential(new PinkNavigate(-140,60,0.5));
+//			//go to base line
+//			addSequential(new WaitCommand(2));
+//			addSequential(new PinkNavigate(-121, -60, 0.7));
+//			addSequential(new PinkNavigate(-121, 0, 0.7));
+//			addSequential(new PinkNavigate(-300, 0, 0.7));
+//			addSequential (new HoldPosition(-300,0,0.7));
 		}
-		/*
-		if(!isBlueAlliance){
-			addSequential(new PinkNavigate(-109,0,1));
-			addSequential(new PinkNavigate(-109,-60,1));
-	        addSequential(new PinkNavigate(-164,-60,1));
-	        //go to base line
-			addSequential(new WaitCommand(2));
-			addSequential(new PinkNavigate(-121, -60, 0.7));
-			addSequential(new PinkNavigate(-121, 0, 0.7));
-			addSequential(new PinkNavigate(-221, 0, 0.7));
-			//addSequential (new StopBase());
-			//addSequential();
-		} else {
-			 //red alliance stuff
-		}
-		*/
 	}
 }
